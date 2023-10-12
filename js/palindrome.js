9872e3b1b0742d66556c18fc88bb5b50504fc86a
CarloVeronese <carlo.veronese97@gmail.com>
2023-10-12 14:14:27 +0200

    Add - control for uppercase

---
+++ js/palindrome.js
@@ -0,0 +1,31 @@
+let word
+
+do{
+    word = (prompt('Inserisci una parola')).toLowerCase
+} while(word == '')
+
+function wordMiddle(word){
+    return Math.floor(word.length / 2)
+}
+
+function areDifferent(elementA, elementB){
+    if(elementA != elementB){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function isPalindrome(word){
+    middlePosition = wordMiddle(word)
+    wordLength = word.length
+    for(let i = 0; i < middlePosition; i++){
+        if(areDifferent(word[i], word[wordLength - 1 - i])){
+            return false
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome(word))
