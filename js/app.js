00692b46864564c66f4d621d203273cb463d4bac
CarloVeronese <carlo.veronese97@gmail.com>
2023-10-12 14:37:55 +0200

    Add - function for even or odd

--- js/app.js
+++ js/app.js
@@ -1,31 +1,35 @@
-let word
-
+let userChoice
 do{
-    word = prompt('Inserisci una parola')
-} while(word == '')
-
-function wordMiddle(word){
-    return Math.floor(word.length / 2)
+    userChoice = (prompt('Scegli pari o dispari')).toLowerCase()
+} while (userChoice != 'pari' && userChoice !='dispari')
+const userNumber = getRndInteger(0, 5)
+const computerNumber = getRndInteger(0, 5)
+const sum = getSum(userNumber, computerNumber)
+console.log('Scelta utente: ' + userChoice + ' Numero utente: ' + userNumber + ' Numero compter: ' + computerNumber + ' Somma: ' + sum)
+if(isEven(sum)){
+    if(userChoice == 'pari') userWinner()
+    else computerWinner()
+}
+else{
+    if(userChoice == 'pari') computerWinner()
+    else userWinner()
+}
+function getRndInteger(min, max){
+    return (Math.floor(Math.random() * (max - min)) + min)
 }
 
-function areDifferent(elementA, elementB){
-    if(elementA != elementB){
-        return true
-    }
-    else{
-        return false
-    }
+function getSum(numA, numB){
+    return numA + numB
 }
 
-function isPalindrome(word){
-    middlePosition = wordMiddle(word)
-    wordLength = word.length
-    for(let i = 0; i < middlePosition; i++){
-        if(areDifferent(word[i], word[wordLength - 1 - i])){
-            return false
-        }
-    }
-    return true;
+function isEven(num){
+    return ((num % 2) == 0)
 }
 
-console.log(isPalindrome(word))
+function userWinner(){
+    console.log("Hai vinto!!")
+}
+
+function computerWinner(){
+    console.log("Ha vinto il computer")
+}
