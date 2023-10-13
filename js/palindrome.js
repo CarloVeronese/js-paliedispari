022b8f4bb01dec66d199ed753da15d5f7111cd0d
CarloVeronese <carlo.veronese97@gmail.com>
2023-10-13 10:05:31 +0200

    remove - useless function

--- js/palindrome.js
+++ js/palindrome.js
@@ -8,20 +8,11 @@ function wordMiddle(word){
     return Math.floor(word.length / 2)
 }
 
-function areDifferent(elementA, elementB){
-    if(elementA != elementB){
-        return true
-    }
-    else{
-        return false
-    }
-}
-
 function isPalindrome(word){
     middlePosition = wordMiddle(word)
     wordLength = word.length
     for(let i = 0; i < middlePosition; i++){
-        if(areDifferent(word[i], word[wordLength - 1 - i])){
+        if(word[i] != word[wordLength - 1 - i]){
             return false
         }
     }
